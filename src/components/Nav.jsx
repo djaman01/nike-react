5fbf5d00fbd437f5753bcc0306500ce2a8a97315
Jaafar Bendahou <jaafar.bendahou@gmail.com>
2024-02-16 11:19:52 +0100

    nouveaux ajoiuts plugin etc

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,51 +1,35 @@
-import { headerLogo } from '../assets/images' //pas la peine de mettre /images/header-logo.svg, car comme on a écrit le meme nom {headerLogo} il va le reconnaitre
-import { hamburger } from '../assets/icons'
-import { navLinks } from '../constants' //import des liens de la navbar avec text
+import { headerLogo } from "../assets/images"; //pas la peine de mettre /images/header-logo.svg, car comme on a écrit le meme nom {headerLogo} il va le reconnaitre
+import { hamburger } from "../assets/icons";
+import { navLinks } from "../constants"; //import des liens de la navbar avec text
 
 const Nav = () => {
   return (
-    <header className='padding-x py-8 absolute z-10 w-full'>
-
-      <nav className='flex justify-between items-center max-container'> {/* max-container est une class qu'on a définit avec @layers @apply dans index.css */}
-
+    <header className="padding-x absolute z-10 w-full py-8">
+      <nav className="max-container flex items-center justify-between">
+        {/* max-container est une class qu'on a définit avec @layers @apply dans index.css */}
         <a href="/">
-          <img
-            src={headerLogo}
-            alt="Nike Logo"
-            width={130}
-            height={29}
-          />
+          <img src={headerLogo} alt="Nike Logo" width={130} height={29} />
         </a>
-
         {/* max-lg: hidden => Max-1024px = hidden / hide on screens smaller than the "lg" breakpoint, which corresponds to a maximum width of 1024 pixels. */}
-        <ul className='flex flex-1 justify-center items-center gap-16 max-lg:hidden'>
+        <ul className="flex flex-1 items-center justify-center gap-16 max-lg:hidden">
           {navLinks.map((item) => (
             <li key={item.label}>
               <a
                 href={item.href}
-                className='font-montserrat leading-normal text-lg text-slate-gray'
+                className="font-montserrat text-lg leading-normal text-slate-gray"
               >
                 {item.label}
               </a>
-
             </li>
           ))}
         </ul>
-
         {/*lg:hidden => >=1024px = hidden donc n'apparait que dans les mobile device ou tablet */}
-        <div className='lg:hidden'>
-          <img
-            src={hamburger}
-            alt="hamburger"
-            width={25}
-            height={25}
-          />
+        <div className="lg:hidden">
+          <img src={hamburger} alt="hamburger" width={25} height={25} />
         </div>
-
       </nav>
-
     </header>
-  )
-}
+  );
+};
 
-export default Nav
+export default Nav;
--- src/sections/Subscribe.jsx
+++ src/sections/Subscribe.jsx
@@ -1,29 +1,25 @@
-import Button from "../components/Button"
+import Button from "../components/Button";
 
 const Subscribe = () => {
-
   return (
     <section
       id="contact-us"
-      className="max-container flex justify-between items-center gap-10 max-lg:flex-col " //max-lg:flex-col: élements seront disposés en colonnes dans mobile devices <1024px
+      className="max-container flex items-center justify-between gap-10 max-lg:flex-col " //max-lg:flex-col: élements seront disposés en colonnes dans mobile devices <1024px
     >
-      <h3 className="text-4xl leading-[68px] font-palanquin font-bold lg:max-w-md">
+      <h3 className="font-palanquin text-4xl font-bold leading-[68px] lg:max-w-md">
         Sign Up for <span className="text-coral-red">Updates</span> & Newletters
       </h3>
       {/* lg:max-w-[40%] + w-full: input et button s'étendent dans tout la partie droite de la section et ne prennent que 40% de cette partie droite But: Laissez un espace blanc entre le h3 et ce div (mettre border et border-black pour bien voir l'effet) */}
       {/* flex: pour que input et bouton soient next to each other et sm:border pour qu'il y ait une bordure à partir de 640px et + */}
-      <div className="lg:max-w-[40%] w-full flex items-center gap-5 p-2.5 rounded-full sm:border sm:border-slate-gray max-sm:flex-col">
-        <input className="input" type="text" placeholder="Your e-mail" /> {/* className: input est défini dans index.css et permet d'enelever les bordures de l'input pour donner un effet comme sur le site avec bouton à coté et bordure qui englove tout */}
-
-        <div className="flex items-center max-sm:justify-center max-sm:w-full">
+      <div className="flex w-full items-center gap-5 rounded-full p-2.5 max-sm:flex-col sm:border sm:border-slate-gray lg:max-w-[40%]">
+        <input className="input" type="text" placeholder="Your e-mail" />{" "}
+        {/* className: input est défini dans index.css et permet d'enelever les bordures de l'input pour donner un effet comme sur le site avec bouton à coté et bordure qui englove tout */}
+        <div className="flex items-center max-sm:w-full max-sm:justify-center">
           <Button label="Sign Up" fullwidth />
         </div>
       </div>
-
-
     </section>
+  );
+};
 
-  )
-}
-
-export default Subscribe
+export default Subscribe;
